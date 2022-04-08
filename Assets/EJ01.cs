b7bbb0d7528bb265c3b67678e58ac5254bf57588
enzomolloy <enzomolloy@gmail.com>
2022-04-08 11:38:20 -0300

    Completo

---
+++ Assets/EJ01.cs
@@ -0,0 +1,28 @@
+﻿//1.Crea un programa que permita ingresar por Inspector dos valores enteros y responda si son iguales o no lo son.
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EJ01 : MonoBehaviour
+{
+    public int num1, num2;
+    void Start()
+    {
+        if (num1 == num2) 
+        {
+            Debug.Log("Los numeros son iguales");
+        }
+
+        else 
+        {
+            Debug.Log("Los numeros no son iguales");
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/EJ02.cs
@@ -0,0 +1,28 @@
+﻿//Crear las variables necesarias para ingresar por Inspector el precio de 3 productos y un monto de dinero disponible.
+//Comunicar si la suma de los mismos supera o no el monto de dinero disponible.Luego indicar cuánto dinero sobra o falta.
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EJ02 : MonoBehaviour
+{
+    public float producto1;
+    public float producto2;
+    public float producto3;
+    public float dinerodisp;
+    float total;
+    void Start()
+    {
+        total = producto1 + producto2 + producto3;
+        if (total <= dinerodisp)
+            Debug.Log("Esta compra es menor el monto y te sobra: " + (dinerodisp - total));
+        else
+            Debug.Log("Esta compra supera tu monto y te falta: " + (total - dinerodisp));
+    }
+
+
+    void Update()
+    {
+
+    }
+}
